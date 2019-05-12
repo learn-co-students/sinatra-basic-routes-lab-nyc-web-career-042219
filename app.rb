31a6eee7422dd9df730860608c0721cb2ebedcf4
Adam Sultanov <adam.sultanov@gmail.com>
2019-05-12 16:56:41 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Adam"
+  end
+
+  get '/hometown' do
+    "My hometown is Pittsburgh"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is \'Moon River\'"
+  end
 end
