3fc97f53c1528fabdea23a29ff235435c20f2e71
virginia koar <virginiakoar@TimKoar.local>
2019-05-12 20:30:30 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Tim"
+  end
+
+  get '/hometown' do
+    "My hometown is Allenhurst"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is reba by phish"
+
+  end
+
 end
